53564c56ec81d8779a07de31e95b8226094a74f5
Rafael Telles <rafael@telles.dev>
2022-07-10 12:25:42 -0300

    Use atomic update on prisma

--- server/api/incrementCounter.ts
+++ server/api/incrementCounter.ts
@@ -4,12 +4,15 @@ const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
     let counter = await prisma.counter.findFirst()
-    counter = await prisma.counter.update({
+    counter = await prisma.counter.upsert({
+        create: {
+            count: 0,
+        },
+        update: {
+            count: { increment: 1 }
+        },
         where: {
             id: counter.id
-        },
-        data: {
-            count: counter.count + 1
         }
     })
     
