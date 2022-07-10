5832524e814e4c06c678a97334df056fd6a01198
Rafael Telles <rafael@telles.dev>
2022-07-10 10:57:41 -0300

    Move old site to Nuxt

--- app.vue
+++ app.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>
-    Hello world!
-  </div>
+  <Head>
+    <Title>Rafael Telles</Title>
+  </Head>
+  <section class="hero is-fullheight has-text-centered">
+    <div class="hero-body">
+      <div class="container" style="max-width: 360px">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image">
+              <img src="~/assets/profile.jpg"/>
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Rafael Telles</p>
+                <p class="subtitle is-6"><a href="mailto:rafael@telles.dev">rafael@telles.dev</a></p>
+              </div>
+            </div>
+
+            <div class="content">
+              Desenvolvedor na <a href="https://tenda.digital" target="_blank">Tenda Digital</a>
+              <br/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style lang="sass">
+@import "node_modules/bulma/bulma.sass"
+
+body
+  background: linear-gradient(50deg, #93c9f5, #c9e1f9)
+</style>
